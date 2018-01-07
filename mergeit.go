24ac037a72674289548a1ddb5521f08ad09b0b35
Peter Darrow <pmdarrow@gmail.com>
2018-01-07 03:51:14 -0400

    Make vars unexported

--- mergeit.go
+++ mergeit.go
@@ -10,11 +10,11 @@ import (
 	"golang.org/x/oauth2"
 )
 
-const RetryTimeout = time.Second * 10
+const retryTimeout = time.Second * 10
 
 var (
-	Info  *log.Logger
-	Error *log.Logger
+	info  *log.Logger
+	error *log.Logger
 
 	// TODO: Is storing these outside main() okay?
 	ctx      context.Context
@@ -23,10 +23,10 @@ var (
 )
 
 func init() {
-	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
-	Error = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime)
+	info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
+	error = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime)
 
-	Info.Println("-- mergeit! --")
+	info.Println("-- mergeit! --")
 
 	ctx = context.Background()
 	tokenSource := oauth2.StaticTokenSource(
@@ -40,39 +40,39 @@ func init() {
 func main() {
 	owner := "pmdarrow"
 	repo := "test"
-	prNum := 8
+	prNum := 9
 	mergeMethod := "squash"
 	mergeit(owner, repo, prNum, mergeMethod)
 }
 
 func mergeit(owner string, repo string, prNum int, mergeMethod string) {
-	Info.Printf("Fetching PR #%v from %v/%v...\n", prNum, owner, repo)
+	info.Printf("Fetching PR #%v from %v/%v...\n", prNum, owner, repo)
 
 	pullRequest, _, err := client.PullRequests.Get(ctx, owner, repo, prNum)
 	if err != nil {
-		Info.Println("Error:", err)
+		info.Println("Error:", err)
 		return
 	}
 
 	if pullRequest.GetMerged() {
-		Info.Printf("Error: PR already merged!")
+		info.Printf("Error: PR already merged!")
 		return
 	}
 
 	if pullRequest.GetMergeableState() == "unknown" {
-		Info.Printf("Merge state unknown, retrying in %v.\n", RetryTimeout)
-		time.Sleep(RetryTimeout)
+		info.Printf("Merge state unknown, retrying in %v.\n", retryTimeout)
+		time.Sleep(retryTimeout)
 		mergeit(owner, repo, prNum, mergeMethod)
 		return
 	}
 
 	if pullRequest.GetMergeableState() == "dirty" {
-		Error.Println("Error: PR has conflicts that must be manually resolved.")
+		error.Println("Error: PR has conflicts that must be manually resolved.")
 		return
 	}
 
 	if pullRequest.GetMergeableState() == "behind" {
-		Info.Println("PR is out-of-date; merging the latest changes from master.")
+		info.Println("PR is out-of-date; merging the latest changes from master.")
 
 		request := &github.RepositoryMergeRequest{
 			// TODO: can't use GetLabel here because it returns string... should I
@@ -83,11 +83,11 @@ func mergeit(owner string, repo string, prNum int, mergeMethod string) {
 		}
 		_, _, err := client.Repositories.Merge(ctx, owner, repo, request)
 		if err != nil {
-			Error.Println("Error:", err)
+			error.Println("Error:", err)
 			return
 		}
-		Info.Printf("PR up-to-date, starting over in %v.\n", RetryTimeout)
-		time.Sleep(RetryTimeout)
+		info.Printf("PR up-to-date, starting over in %v.\n", retryTimeout)
+		time.Sleep(retryTimeout)
 		mergeit(owner, repo, prNum, mergeMethod)
 		return
 	}
@@ -96,29 +96,29 @@ func mergeit(owner string, repo string, prNum int, mergeMethod string) {
 		statuses, _, err := client.Repositories.GetCombinedStatus(
 			ctx, owner, repo, *pullRequest.Head.SHA, &listOpts)
 		if err != nil {
-			Error.Println("Error:", err)
+			error.Println("Error:", err)
 			return
 		}
 
 		if statuses.GetState() == "success" {
-			Error.Println("Error: PR up-to-date and build passed, but still can't be merged.")
+			error.Println("Error: PR up-to-date and build passed, but still can't be merged.")
 		}
 
 		if statuses.GetState() == "failed" {
-			Error.Println("Error: Build failed and must be manually fixed.")
+			error.Println("Error: Build failed and must be manually fixed.")
 			return
 		}
 
 		if statuses.GetState() == "pending" {
-			Info.Printf("Build in progress. Retrying in %v.\n", RetryTimeout)
-			time.Sleep(RetryTimeout)
+			info.Printf("Build in progress. Retrying in %v.\n", retryTimeout)
+			time.Sleep(retryTimeout)
 			mergeit(owner, repo, prNum, mergeMethod)
 			return
 		}
 	}
 
 	if pullRequest.GetMergeableState() == "clean" {
-		Info.Printf("Ready to be merged! Merging with the \"%v\" method.\n", mergeMethod)
+		info.Printf("Ready to be merged! Merging with the \"%v\" method.\n", mergeMethod)
 		// TODO: Provide proper squash message here
 		message := "Merged by mergeit"
 
@@ -127,10 +127,10 @@ func mergeit(owner string, repo string, prNum int, mergeMethod string) {
 		}
 		_, _, err := client.PullRequests.Merge(ctx, owner, repo, prNum, message, opts)
 		if err != nil {
-			Error.Println("Error:", err)
+			error.Println("Error:", err)
 			return
 		}
 
-		Info.Println("PR successfully merged.")
+		info.Println("PR successfully merged.")
 	}
 }
